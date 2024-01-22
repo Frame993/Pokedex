27e0c5eeecbd16e243e0b2f613f2f5fc4bf24193
Yordani <yalvarezb93@gmail.com>
2024-01-22 17:07:35 +0300

    Javascript functional with API and  eventListeners

---
+++ js/main.js
@@ -0,0 +1,75 @@
+const listaPokemon = document.querySelector('#listaPokemon');
+const botonesHeader = document.querySelectorAll('.btn-header');
+let URL = "https://pokeapi.co/api/v2/pokemon/";
+
+for (let i = 1; i <= 151; i++) {
+    fetch(URL + i)
+        .then((response) => response.json())
+        .then((data) => mostrarPokemon(data))
+}
+
+function mostrarPokemon(poke) {
+
+    let tipos = poke.types.map((type) => `<p class="${type.type.name} tipo">${type.type.name}</p>`);
+    tipos = tipos.join('');
+
+    let pokeId = poke.id.toString()
+    if (pokeId.length == 1) {
+        pokeId = '00' + pokeId
+    } else if (pokeId.length == 2) {
+        pokeId = '0' + pokeId
+    }
+
+    const div = document.createElement('div');
+    div.classList.add('pokemon');
+    div.innerHTML = `
+    <p class="pokemon-id-back">${pokeId}</p>
+    <div class="pokemon-image">
+        <img src="${poke.sprites.other['official-artwork'].front_default}"
+            alt="${poke.name}">
+    </div>
+    <div class="pokemon-info">
+        <div class="nombre-contenedor">
+            <p class="pokemon-id">#${pokeId}</p>
+            <h2 class="pokemon-nombre">${poke.name}</h2>
+        </div>
+        <div class="pokemon-tipos">
+            ${tipos}
+        </div>
+        <div class="pokemon-stats">
+            <P class="stat">${poke.height}m</P>
+            <p class="stat">${poke.weight}kg</p>
+        </div>
+    </div>
+    `;
+    listaPokemon.appendChild(div);
+}
+
+// Para selecionar especificamente el botón que se ha pulsado se puede hacer de la siguiente manera
+
+botonesHeader.forEach(boton => {
+    boton.addEventListener('click', (event) => {
+        const botonId = event.currentTarget.id
+
+        listaPokemon.innerHTML = "";
+
+        for (let i = 1; i <= 151; i++) {
+            fetch(URL + i)
+                .then((response) => response.json())
+                .then((data) => {
+
+                    if (botonId === 'ver-todos') {
+                        mostrarPokemon(data)
+                        return
+                    } else {
+                        const tipos = (data.types.map(type => type.type.name));
+                        if (tipos.some(tipo => tipo.includes(botonId)))
+                            mostrarPokemon(data)
+                    }
+
+
+                })
+        }
+    })
+
+})
